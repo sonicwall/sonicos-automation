a51bec69af36297773addfa08407e4aac08ba4af
Jaime Escalera <jescalera@sonicwall.com>
2025-10-23 05:57:44 -0700

    Prepping for web app.

--- credential_reset/web/js/forms.js
+++ credential_reset/web/js/forms.js
@@ -701,7 +701,7 @@ function displaySummaryData(summaryData) {
                                         Priority
                                     </th>
                                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Configuration Area
+                                        Config Area
                                     </th>
                                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Finding
